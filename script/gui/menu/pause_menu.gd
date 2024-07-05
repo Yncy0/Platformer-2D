566bc9ed8b96403a315a972d534f949a7cc8351a
Yncy0 <paalayancy@gmail.com>
2024-07-05 22:26:49 +0800

    add buttons functionality -> pause menu

--- script/gui/menu/pause_menu.gd
+++ script/gui/menu/pause_menu.gd
@@ -1,12 +1,9 @@
 extends Control
 
 
+@onready var starting_menu: PackedScene = preload("res://scene/gui/menu/starting_menu.tscn")
 @onready var settings_menu: Control = $SettingsMenu
 @onready var pause_panel: Panel = $PausePanel
-
-
-func _ready() -> void:
-	pass
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -16,7 +13,24 @@ func _unhandled_input(event: InputEvent) -> void:
 	elif event.is_action_pressed("pause") and pause_panel.visible == true:
 		pause_panel.visible = false
 		get_tree().paused = false
+	
+	if event.is_action("pause") and settings_menu.visible == true:
+		pause_panel.visible = false
+		settings_menu.visible = false
+		get_tree().paused = false
 
 
-func _process(_delta: float) -> void:
-	pass
+func _on_settings_button_pressed() -> void:
+	pause_panel.visible = false
+	settings_menu.visible = true
+
+
+
+func _on_back_button_pressed() -> void:
+	pause_panel.visible = false
+	get_tree().paused = false
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().call_deferred("change_scene_to_packed", starting_menu)
