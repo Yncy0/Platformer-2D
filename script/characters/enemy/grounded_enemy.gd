69291847450db1dbca2205b29a836b0de555f2e9
Yncy0 <paalayancy@gmail.com>
2024-07-05 16:00:51 +0800

    add audio

--- script/characters/enemy/grounded_enemy.gd
+++ script/characters/enemy/grounded_enemy.gd
@@ -3,8 +3,12 @@ extends CharacterBody2D
 
 @export var SPEED = 300.0
 
+
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction:int = 1
 
 
 func _physics_process(delta: float) -> void:
@@ -12,17 +16,17 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-
-	var direction = 1
 	
-	if is_on_wall():
+	if is_on_wall() or ray_cast_2d.is_colliding() == false:
 		direction = -1
+	else:
+		direction = 1
 	
-	if direction:
-		velocity.x = direction * SPEED
+	
+	velocity.x = direction * SPEED
 	
 	
-		
+	
 
 	move_and_slide()
 
