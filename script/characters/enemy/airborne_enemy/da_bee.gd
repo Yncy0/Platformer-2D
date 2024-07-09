b92440187412f935eadfb889db2e2a95c55089a5
Yncy0 <paalayancy@gmail.com>
2024-07-09 13:06:52 +0800

    test dabee

--- script/characters/enemy/airborne_enemy/da_bee.gd
+++ script/characters/enemy/airborne_enemy/da_bee.gd
@@ -7,6 +7,7 @@ class_name DaBee extends AirborneEnemy
 
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,7 +17,10 @@ func _ready() -> void:
 	animated_sprite_2d.play("idle")
 
 
-func _physics_process(delta: float) -> void:
-	
+func _physics_process(_delta: float) -> void:
+	if ray_cast_2d.is_colliding():
+		var r = ray_cast_2d.get_collider()
+		if r is Player:
+			print("YOU PRICK!")
 	
 	move_and_slide()
