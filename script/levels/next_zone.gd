25123fc3d8365717e4af16c1683de67588aab4dd
Yncy0 <paalayancy@gmail.com>
2024-07-15 10:44:06 +0800

    finish: prototyping loading scene

--- script/levels/next_zone.gd
+++ script/levels/next_zone.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 
-
+@export var next_scene: String
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
+		SceneManager.next_scene = self.next_scene
 		get_tree().call_deferred("change_scene_to_packed", SceneManager.loading_scene)
